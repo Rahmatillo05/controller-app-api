632948367e3c777da81db08d92c292344e50c95b
RAHMATILLOo3o5 <husanboyev2oo5.com>
2023-03-04 23:57:14 +0500

    change and create some files

--- config/web.php
+++ config/web.php
@@ -8,6 +8,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'timeZone' => 'Asia/Tashkent',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
