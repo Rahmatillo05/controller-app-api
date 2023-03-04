dce4499f16cb278fe764d888efb8aa1b03127b5e
RAHMATILLOo3o5 <husanboyev2oo5.com>
2023-03-04 18:53:49 +0500

    edit

---
+++ migrations/m230304_135323_create_category_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m230304_135323_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%category}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%category}}');
+    }
+}
