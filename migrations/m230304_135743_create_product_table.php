3398e496f6958ba2b12a8d25acbd3eb2194ffb4c
RAHMATILLOo3o5 <husanboyev2oo5.com>
2023-03-04 18:58:34 +0500

    edit

---
+++ migrations/m230304_135743_create_product_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%product}}`.
+ */
+class m230304_135743_create_product_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%product}}', [
+            'id' => $this->primaryKey(),
+
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%product}}');
+    }
+}
