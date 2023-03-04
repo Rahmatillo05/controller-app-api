021363da62675cb17862c8b20cf6901e675f028d
RAHMATILLOo3o5 <husanboyev2oo5.com>
2023-03-05 00:01:51 +0500

    change and create some files

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -17,11 +17,7 @@ class AuthController extends Controller
         $model = new AdminLoginForm();
 
         if ($this->request->isPost) {
-            if ($model->load(Yii::$app->request->post(), '') && $user_data = $model->login()) {
-                return $user_data;
-            } else {
-                return $model->errors;
-            }
+            return $this->request->post();
         } else {
             throw new MethodNotAllowedHttpException("Method Not Allowed. This URL can only handle the following request methods: POST.");
         }
