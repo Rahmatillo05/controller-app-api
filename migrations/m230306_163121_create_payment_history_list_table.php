ecae465ea6809b0212907507302626c166248b9f
RAHMATILLOo3o5 <husanboyev2oo5.com>
2023-03-07 21:47:51 +0500

    change and create some files

--- migrations/m230306_163121_create_payment_history_list_table.php
+++ migrations/m230306_163121_create_payment_history_list_table.php
@@ -14,11 +14,11 @@ class m230306_163121_create_payment_history_list_table extends Migration
     {
         $this->createTable('{{%payment_history_list}}', [
             'id' => $this->primaryKey(),
-            'history_id' => $this->integer(),
+            'debtor_id' => $this->integer(),
             'pay_amount' => $this->integer(),
             'created_at' => $this->integer(),
         ]);
-        $this->addForeignKey('fk-payment_history_list-to-payment_history', 'payment_history_list', 'history_id', 'payment_history','id', 'CASCADE');
+        $this->addForeignKey('fk-payment_history_list-to-debtor', 'payment_history_list', 'debtor_id', 'debtor', 'id', 'CASCADE');
     }
 
     /**
