b1bd79506f4036424796bcc3e201281880bf5290
RAHMATILLOo3o5 <husanboyev2oo5.com>
2023-03-09 21:54:12 +0500

    change and create some files

--- migrations/m230304_095211_create_user_table.php
+++ migrations/m230304_095211_create_user_table.php
@@ -7,43 +7,54 @@ use yii\db\Migration;
  */
 class m230304_095211_create_user_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-        $this->createTable('{{%user}}', [
-            'id' => $this->primaryKey(),
-            'first_name' => $this->string(150)->notNull(),
-            'last_name' => $this->string(150)->notNull(),
-            'username' => $this->string(150)->unique()->notNull(),
-            'password' => $this->string(150)->notNull(),
-            'phone_number' => $this->string(150)->notNull(),
-            'address' => $this->string(150)->notNull(),
-            'auth_key' => $this->string(250),
-            'user_role' => $this->smallInteger()->defaultValue(10),
-            'status' => $this->smallInteger()->defaultValue(10),
-            'created_at' => $this->integer(),
-            'updated_at' => $this->integer(),
-        ]);
-        $this->insert('user', [
-            'first_name' => 'admin',
-            'last_name' => 'admin',
-            'username' => 'admin',
-            'address' => 'Xonobod',
-            'password' => Yii::$app->security->generatePasswordHash('admin'),
-            'auth_key' => Yii::$app->security->generateRandomString(15),
-            'phone_number' => "+998908385438",
-            'created_at' => time(),
-            'updated_at' => time(),
-        ]);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp ()
+	{
+		$this->createTable('{{%user}}', [
+			'id' => $this->primaryKey(),
+			'first_name' => $this->string(150)->notNull(),
+			'last_name' => $this->string(150)->notNull(),
+			'username' => $this->string(150)->unique()->notNull(),
+			'password' => $this->string(150)->notNull(),
+			'phone_number' => $this->string(150)->notNull(),
+			'address' => $this->string(150)->notNull(),
+			'auth_key' => $this->string(250),
+			'user_role' => $this->smallInteger()->defaultValue(10),
+			'status' => $this->smallInteger()->defaultValue(10),
+			'created_at' => $this->integer(),
+			'updated_at' => $this->integer(),
+		]);
+		$this->insert('user', [
+			'first_name' => 'admin',
+			'last_name' => 'admin',
+			'username' => 'admin',
+			'address' => 'admin',
+			'password' => Yii::$app->security->generatePasswordHash('admin'),
+			'auth_key' => Yii::$app->security->generateRandomString(15),
+			'phone_number' => "+998999999999",
+			'created_at' => time(),
+			'updated_at' => time(),
+		]);
+		$this->insert('user', [
+			'first_name' => 'seller',
+			'last_name' => 'seller',
+			'username' => 'seller',
+			'address' => 'seller',
+			'password' => Yii::$app->security->generatePasswordHash('seller'),
+			'auth_key' => Yii::$app->security->generateRandomString(15),
+			'phone_number' => "+998999999999",
+			'created_at' => time(),
+			'updated_at' => time(),
+		]);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        $this->dropTable('{{%user}}');
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown ()
+	{
+		$this->dropTable('{{%user}}');
+	}
 }
