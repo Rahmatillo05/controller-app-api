64f66fbf46473d4504976c795d49bad47d8d7729
RAHMATILLOo3o5 <husanboyev2oo5.com>
2023-03-11 14:07:57 +0500

    change and create some files

--- models/Statistics.php
+++ models/Statistics.php
@@ -55,17 +55,27 @@ class Statistics extends \yii\db\ActiveRecord
 
 	private function calculateSpent ()
 	{
+		$product_spent = Product::find()->sum('purchase_price') ?? 0;
+		$other_spent = OtherSpent::find()->sum('sum') ?? 0;
 
+		return $product_spent + $other_spent;
 	}
 
 	private function calculateBenefit ()
 	{
-
+		return Selling::find()->sum('sell_price') ?? 0;
 	}
 
 	private function calculatePureBenefit ()
 	{
-
+		$sell_online = Selling::find()->where([ 'type_sell' => Selling::PAY_ONLINE ])->sum('sell_price') ?? 0;
+		$plastic_card_tax = PlasticCardTax::find()->orderBy([ 'id' => SORT_DESC ])->one();
+		$plastic_spent = $sell_online * ( $plastic_card_tax / 100 );
+		$sell_debt = Selling::find()->where([ 'type_sell' => Selling::PAY_DEBT ])->sum('sell_price');
+		$pay_instant = DebtHistory::find()->sum('pay_amount') ?? 0;
+		$pay_history_amount = PaymentHistoryList::find()->sum('pay_amount') ?? 0;
+		$debt = $sell_debt - $pay_instant + $pay_history_amount;
+		return $this->calculateBenefit() - ( $this->calculateSpent() + $plastic_spent + $debt );
 	}
 
 }
