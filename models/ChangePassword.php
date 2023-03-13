0cc2c6d40cd7a4f160d977fc776524a5a9767233
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-03-13 23:33:38 +0500

    edit ChangePassword.php file

--- models/ChangePassword.php
+++ models/ChangePassword.php
@@ -9,40 +9,45 @@ use yii\base\Model;
 class ChangePassword extends Model
 {
 
-	public $old_password;
-	public $new_password;
-	public $confirm_password;
+    public $old_password;
+    public $new_password;
+    public $confirm_password;
 
-	public function rules ()
-	{
-		return [
-			[ [ 'old_password', 'new_password', 'confirm_password' ], 'required' ],
-			[ [ 'old_password', 'new_password', 'confirm_password' ], 'string', 'length' => [ 8, 16 ] ],
-			[ [ 'confirm_password' ], 'compare', 'compareAttribute' => 'new_password', 'message' => "{attribute} maydoni teng bo'lishi kerak {compareAttribute} maydoniga" ]
-		];
-	}
+    public $new_username;
 
-	public function attributeLabels ()
-	{
-		return [
-			'old_password' => 'Amaldagi parol',
-			'new_password' => 'Yanngi parol',
-			'confirm_password' => 'Parolni tasdiqlash',
-		];
-	}
+    public function rules()
+    {
+        return [
+            [['old_password', 'new_password', 'confirm_password', 'new_username'], 'required'],
+            [['old_password', 'new_password', 'confirm_password'], 'string', 'length' => [8, 16]],
+            [['new_username'], 'string', 'min' => 3],
+            [['confirm_password'], 'compare', 'compareAttribute' => 'new_password', 'message' => "{attribute} maydoni teng bo'lishi kerak {compareAttribute} maydoniga"]
+        ];
+    }
 
-	public function save (): bool
-	{
-		$user = User::findOne([ 'id' => Yii::$app->user->id ]);
+    public function attributeLabels()
+    {
+        return [
+            'username' => 'Foydalanuvchi nomi',
+            'old_password' => 'Amaldagi parol',
+            'new_password' => 'Yanngi parol',
+            'confirm_password' => 'Parolni tasdiqlash',
+        ];
+    }
 
-		if ( $user != null ) {
-			if ( $user->validatePassword($this->old_password) ) {
-				$user->setPassword($this->new_password);
-				return $user->save();
-			} else {
-				$this->addError($this->old_password, 'Parol noto\'g\'ri.');
-			}
-		}
-		return false;
-	}
+    public function save(): bool
+    {
+        $user = User::findOne(['id' => Yii::$app->user->id]);
+
+        if ($user != null) {
+            if ($user->validatePassword($this->old_password)) {
+                $user->setPassword($this->new_password);
+                $user->username = $this->new_username;
+                return $user->save();
+            } else {
+                $this->addError($this->old_password, 'Parol noto\'g\'ri.');
+            }
+        }
+        return false;
+    }
 }
