d10f6421c768e476399b90e2ea7250ea575bd51a
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-03-23 11:40:12 +0500

    edit some files

--- models/Product.php
+++ models/Product.php
@@ -11,14 +11,11 @@ use yii\behaviors\TimestampBehavior;
  * @property int $id
  * @property int|null $category_id
  * @property string $product_name
- * @property int $amount
- * @property int $each_amount
  * @property int $all_amount
  * @property int $purchase_price
  * @property int $wholesale_price
  * @property int $retail_price
  * @property int|null $created_at
- *
  * @property Category $category
  */
 class Product extends \yii\db\ActiveRecord
@@ -47,8 +44,8 @@ class Product extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['category_id', 'amount', 'each_amount', 'all_amount', 'purchase_price', 'wholesale_price', 'retail_price', 'created_at'], 'integer'],
-            [['product_name', 'amount', 'each_amount', 'all_amount', 'purchase_price', 'wholesale_price', 'retail_price'], 'required'],
+            [['category_id', 'all_amount', 'purchase_price', 'wholesale_price', 'retail_price', 'created_at'], 'integer'],
+            [['product_name',  'all_amount', 'purchase_price', 'wholesale_price', 'retail_price'], 'required'],
             [['product_name'], 'string', 'max' => 200],
             [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::class, 'targetAttribute' => ['category_id' => 'id']],
         ];
@@ -63,8 +60,6 @@ class Product extends \yii\db\ActiveRecord
             'id' => 'ID',
             'category_id' => 'Category ID',
             'product_name' => 'Product Name',
-            'amount' => 'Amount',
-            'each_amount' => 'Each Amount',
             'all_amount' => 'All Amount',
             'purchase_price' => 'Purchase Price',
             'wholesale_price' => 'Wholesale Price',
@@ -81,8 +76,6 @@ class Product extends \yii\db\ActiveRecord
                 return $this->category;
             },
             'product_name',
-            'amount',
-            'each_amount',
             'all_amount' => function () {
                 return $this->productRemain();
             },
