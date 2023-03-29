16a28249860126f51f9de03438c4cc39a0ea2b2f
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-03-29 20:56:41 +0500

    edit some files

--- controllers/StatisticsController.php
+++ controllers/StatisticsController.php
@@ -8,13 +8,18 @@ class StatisticsController extends BaseController
 {
     public $modelClass = Statistics::class;
 
-    public function actions()
+    public function actions(): array
     {
         $actions = parent::actions();
 
         unset($actions['create'], $actions['update'], $actions['delete'], $actions['view']);
 
         return $actions;
+    }
+
+    public function actionCreate()
+    {
+        return "Save";
     }
 
 }
