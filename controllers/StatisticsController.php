6c47b1ed7e9899358e828064a1aae38ff47e58e0
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-03-29 23:18:28 +0500

    edit some files

--- controllers/StatisticsController.php
+++ controllers/StatisticsController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use app\models\Statistics;
 use app\models\StatisticsDetail;
+use Yii;
+use yii\data\ActiveDataProvider;
 
 class StatisticsController extends BaseController
 {
@@ -14,8 +16,21 @@ class StatisticsController extends BaseController
         $actions = parent::actions();
 
         unset($actions['create'], $actions['update'], $actions['delete'], $actions['view']);
-
+        $actions['index']['prepareDataProvider'] = [$this, 'data'];
         return $actions;
+    }
+
+    public function data()
+    {
+        $monthNumber = Yii::$app->request->get('month');
+        $query = Statistics::find();
+        if ($monthNumber) {
+            $query->where(['MONTH(period)' => $monthNumber]);
+        }
+        return new ActiveDataProvider([
+            'query' => $query
+        ]);
+
     }
 
     public function actionCreate(): bool
