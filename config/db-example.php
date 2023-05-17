c5a0cc6363697aaf791562df7213f6b5e75ccea7
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-05-17 21:00:46 +0500

    new edition

---
+++ config/db-example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u294704900_api',
+    'username' => 'u294704900_root',
+    'password' => 'Rahmatillo05@',
+    'charset' => 'utf8',
+];
