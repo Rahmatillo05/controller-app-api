0421b54c72a5cc9d10d418c5452f679de407721d
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-05-17 20:55:30 +0500

    edit

--- config/db.php
+++ config/db.php
@@ -5,10 +5,5 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=u294704900_api',
     'username' => 'u294704900_root',
     'password' => 'Rahmatillo05@',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'charset' => 'utf8'
 ];
