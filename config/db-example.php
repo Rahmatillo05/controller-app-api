3d00b1c94c1313be78a5c72c7318041fd4c51c1f
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-05-17 21:02:00 +0500

    edit

--- config/db-example.php
+++ config/db-example.php
@@ -1,5 +1,9 @@
 <?php
-
+/*
+ * 1. Add new file as db.php
+ * 2. Write this code in new db.php
+ * 3. All done! :)
+ */
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=u294704900_api',
